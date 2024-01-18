f10390fc93aebba4571ddabec9560014f272582c
dohaelsayedrabea <155635179+dohaelsayedrabea@users.noreply.github.com>
2024-01-18 16:33:23 +0200

    task3

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+(function($) {
+    "use strict";
+    /*
+  < !-- ============================================================== -->
+  < !--Password Toggle js -- >
+  < !-- ============================================================== -->
+*/
+    $(".toggle-password").click(function() {
+
+        $(this).toggleClass("fa-lock fa-lock-open");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+    /*
+  < !-- ============================================================== -->
+  < !--Header Mobile -- >
+  < !-- ============================================================== -->
+*/
+    (function($) {
+        $.fn.grtmobile = function() {
+            $('.grt-mobile-button').on('click', function() {
+                $(this).toggleClass("grt-mobile-button-open");
+                $("ul.grt-menu").toggleClass("open-grt-menu ");
+                $("html, body").toggleClass("body-overflow");
+            });
+            $('li.grt-dropdown').on('click', function(e) {
+                $(this).toggleClass("active-dropdown");
+            });
+        }
+    })(jQuery);
+
+    // Initialize and check for mobile
+    $.fn.grtmobile();
+
+
+})(jQuery);
